22f9b810e0998a507748e7b436a5082d1040e0be
Quartz <skyu.dev@gmail.com>
2025-01-30 00:44:48 +0900

    feat: add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,26 @@ export const metadata = {
   title: "Who is Quartz?",
   description:
     "Test your ability to identify Quartz in this exciting photo quiz!",
+  openGraph: {
+    title: "Who is Quartz?",
+    description:
+      "Test your ability to identify Quartz in this exciting photo quiz!",
+    images: [
+      {
+        url: "/opengraph-image.png", // 상대 경로 또는 절대 URL
+        width: 1200,
+        height: 630,
+        alt: "Quartz Quiz Open Graph Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Who is Quartz?",
+    description:
+      "Test your ability to identify Quartz in this exciting photo quiz!",
+    images: ["/twitter-image.png"],
+  },
 };
 
 export default function RootLayout({
